9b892f4a6ed882e39e5b0c73846ef0b8c15c5722
Cheche0802 <jmonterrey1993@gmail.com>
2023-05-31 21:35:21 -0400

    inicializacion de calendario video 22

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -1,1 +1,29 @@
-Hola mucho
+<x-app-layout>
+
+    <script src="{{asset('fullcalendar/core/index.global.js')}}"></script>
+    <script src="{{asset('fullcalendar/interaction/index.global.js')}}"></script>
+    <script src="{{asset('fullcalendar/daygrid/index.global.js')}}"></script>
+    <script src="{{asset('fullcalendar/google-calendar/index.global.js')}}"></script>
+    <script src="{{asset('fullcalendar/icalendar/index.global.js')}}"></script>
+    <script src="{{asset('fullcalendar/interaction/index.global.js')}}"></script>
+    <script src="{{asset('fullcalendar/list/index.global.js')}}"></script>
+    <script src="{{asset('fullcalendar/timegrid/index.global.js')}}"></script>
+    <script>
+
+        document.addEventListener('DOMContentLoaded', function() {
+          var calendarEl = document.getElementById('calendar');
+          var calendar = new FullCalendar.Calendar(calendarEl, {
+            timeZone: 'local',
+            initialView: 'dayGridMonth'
+          });
+          calendar.setOption('locale', 'Ve');
+          calendar.render();
+        });
+
+      </script>
+
+          <div class="mx-auto max-w-5xl px-2 sm:px-6 lg:px-8 py-8" id="calendar">Calendario... </div>
+
+
+
+</x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @yield('scripts')
 
         <!-- Styles -->
         @livewireStyles
