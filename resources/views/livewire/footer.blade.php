40b0cc672cb372c882b3b42b7ef4b6f7864d5904
eDiazCW <ediaz@code-war.com>
2023-06-13 17:11:56 -0400

    footer y navbar
    agregados el logo y cambios en el tamano

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,20 +1,21 @@
-<nav class="bg-white-800" x-data="{ open: false }">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+<nav class="bg-cyan-800 " x-data="{ open: false }" style="height: 5rem;">
+    <div class="mx-0 max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
 
-            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start pt-3">
+            {{-- <div class="position-absolute top-100 start-100 translate-middle" style="border: red 1px solid"> --}}
                 {{-- Logotipo --}}
-                <a href="/" class="flex flex-shrink-0 items-center">
+                {{-- <a href="/" class="flex flex-shrink-0 items-center "> --}}
+                <a href="/" class="d-flex align-items-center">
                     <img class="block  mt-5 w-auto lg:hidden"
                         src="storage/logos/LOGOsin fondo horizontal.png" alt="Comunidad de gracia">
-                    <img class="hidden  w-auto lg:block"
-                        src="storage/logos/LOGOsin fondo horizontal.png" alt="Comunidad de gracia">
+                    <img class="hidden w-auto lg:block" style="height: 65px;"
+                        src="storage/logos/LOGOsinFondoHorizontal.png" alt="Comunidad de gracia">
                 </a>
                 {{-- Menu LG --}}
                 <div class="hidden sm:ml-6 sm:block">
                     <div class="flex space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-
                     </div>
                 </div>
             </div>
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -10,20 +10,20 @@
                     aria-controls="mobile-menu" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <!--
-              Icon when menu is closed.
+                            Icon when menu is closed.
 
-              Menu open: "hidden", Menu closed: "block"
-            -->
+                            Menu open: "hidden", Menu closed: "block"
+                        -->
                     <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                     <!--
-              Icon when menu is open.
+                        Icon when menu is open.
 
-              Menu open: "block", Menu closed: "hidden"
-            -->
+                        Menu open: "block", Menu closed: "hidden"
+                    -->
                     <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -35,15 +35,15 @@
                 {{-- Logotipo --}}
                 <a href="/" class="flex flex-shrink-0 items-center">
                     <img class="block  mt-5 w-auto lg:hidden" src="storage/logos/logo.png" alt="Comunidad de gracia">
-                    <img class="hidden h-10 w-auto lg:block" src="storage/logos/logo.png" alt="Comunidad de gracia">
-            </a>
+                    <img class="hidden h-16 w-auto lg:block" src="storage/logos/logo.png" alt="Comunidad de gracia">
+                </a>
                 {{-- Menu LG --}}
-                <div class="hidden sm:ml-6 sm:block">
-                    <div class="flex space-x-4">
+                <div class="hidden sm:ml-6 sm:block ">
+                    <div class="flex space-x-4 ">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         @foreach ($categories as $category)
                             <a href="{{ route('posts.category', $category) }}"
-                                class="text-black-600 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">
+                                class="text-black-600 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-5 text-base font-medium">
                                 {{ $category->name }} </a>
                         @endforeach
                     </div>
@@ -53,20 +53,18 @@
             @auth
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     {{-- Boton Notificacion --}}
-                    @if( $count = Auth::user()->unreadNotifications->count())
+                    @if ($count = Auth::user()->unreadNotifications->count())
+                        <a href="{{ route('notifications.index') }}" type="button"
+                            class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <span class="sr-only">View notifications</span>
 
-                    <a href="{{route('notifications.index')}}" type="button"
-                    class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span class="sr-only">View notifications</span>
-
-                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                            aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                        </svg>
-                            {{$count}}
-                    </a>
-
+                            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                            </svg>
+                            {{ $count }}
+                        </a>
                     @endif
 
 
@@ -129,11 +127,11 @@
             @endforeach
 
             <a href="{{ route('login') }}"
-            class="bg-orange-600 text-white block rounded-md px-3 py-2 text-base font-medium"
-            aria-current="page">Login</a>
+                class="bg-orange-600 text-white block rounded-md px-3 py-2 text-base font-medium"
+                aria-current="page">Login</a>
             <a href="{{ route('register') }}"
-            class="bg-blue-300 text-white block rounded-md px-3 py-2 text-base font-medium"
-            aria-current="page">Registro</a>
+                class="bg-blue-300 text-white block rounded-md px-3 py-2 text-base font-medium"
+                aria-current="page">Registro</a>
         </div>
 
 
